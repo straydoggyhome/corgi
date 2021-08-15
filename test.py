e0500a7747290c8b79dec7419b69114187388efc
feiandxs <yiziyint@gmail.com>
2021-08-15 14:21:05 +0800

    first

---
+++ test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ibegyourpardon
+from wsgiref.simple_server import make_server
+def application(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/html')])
+    return [b'<h1>Hello, web!</h1>']
+if __name__ == '__main__':
+    httpd = make_server('', 9999, application)
+    print("Serving HTTP on port 9999...")
+    httpd.serve_forever()
