1fad4d83e89a09b38b64b6fa9b6ec8539ad35b4a
Your Name <you@example.com>
2021-08-16 21:04:00 +0800

    save

--- test.py
+++ test.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # ibegyourpardon
-from wsgiref.simple_server import make_server
-def application(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    return [b'<h1>Hello, web!</h1>']
-if __name__ == '__main__':
-    httpd = make_server('', 9999, application)
-    print("Serving HTTP on port 9999...")
-    httpd.serve_forever()
+# from wsgiref.simple_server import make_server
+
+
+def app(environ, start_response):
+    print(environ)
+    print(type(environ))
+    data = b"Hello, World!\n"
+    start_response("200 OK", [
+        ("Content-Type", "text/plain"),
+        ("Content-Length", str(len(data)))
+    ])
+    return iter([data])
+
+# if __name__ == '__main__':
+#     httpd = make_server('', 9999, application)
+#     print("Serving HTTP on port 9999...")
+#     httpd.serve_forever()
